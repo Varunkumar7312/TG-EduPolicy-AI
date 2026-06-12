110dfe53407b538df38b322a36a74f0acc24150d
Varunkumar7312 <varuneslavath96@gmail.com>
2026-06-12 23:35:29 +0530

    Add preview configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 10000,
+    allowedHosts: [
+      'tg-edupolicy-ai.onrender.com'
+    ]
+  }
 })
